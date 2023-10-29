ac778ec40ecdae53cb485989885d90b6dcbc4605
mrznaboya <zuyax@yahoo.com>
2023-10-29 12:20:29 +0800

    Passing Callbacks to Children

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import ColorCounter from "../components/ColorCounter";
 
 const SquareScreen = () => {
-  return <Text>Square Screen </Text>;
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
+  //   console.log(red);
+
+  return (
+    <View>
+      <ColorCounter
+        onIncrease={() => setRed(red + 1)}
+        onDecrease={() => setRed(red - 1)}
+        color="Red"
+      />
+      <ColorCounter color="Green" />
+      <ColorCounter color="Blue" />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
